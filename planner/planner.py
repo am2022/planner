4b80b12c9b03109afac2f063fa939a273f189dfd
am <am2022a2@gmail.com>
2022-08-18 08:59:05 +0430

    add getting the number of an item in a list option

--- planner/planner.py
+++ planner/planner.py
@@ -1,6 +1,6 @@
 print("welcome to planner")
 print("enter -q for exit || enter -l for see list || enter -d for delete an item")
-print("enter -del for delete all the list")
+print("enter -del for delete all the list || enter -numl for number of a work in the list")
 
 todo = []
 
@@ -51,6 +51,23 @@ while b:
         else:
             todo.clear()
             print("done!")
+
+    elif do == "-numl":
+        if todo == []:
+            print("\nthe list is empty!\n")
+        else:
+            numl_work = input("enter the work: ")
+            is_numl_valid = True
+
+            try:
+                numl = todo.index(numl_work)
+            except:
+                print("the work is not valid!")
+                is_numl_valid = False
+
+            if is_numl_valid:
+                numl += 1
+                print("the number of the work is in "+str(numl))
 
     else:
         todo.append(do)
